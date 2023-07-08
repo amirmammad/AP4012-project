16e235317dc1af2d880d12ac1bbb64f939c41eb8
amirmammad <amr.mrshms83@gmail.com>
2023-07-08 19:27:18 +0330

    added digikala and ready to make the search page

---
+++ Digikala.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+def search(target, type):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
+    
+    if type == 'mobile':
+        driver.get(f'https://www.digikala.com/search/mobile/?q={target}')
+    elif type == 'laptop':
+        driver.get(f'https://www.digikala.com/search/laptop/?q={target}')
+    elif type == 'motorcycle':
+        driver.get(f'https://www.digikala.com/search/motorbike/?q={target}')
+    elif type == 'tv':
+        driver.get(f'https://www.digikala.com/search/tv2/?q={target}')
+    elif type == 'audio':
+        driver.get(f'https://www.digikala.com/search/home-audio-systems/?q={target}')
+    
+    sleep(3)
+    search_table = driver.find_element(By.XPATH, '//*[@id="ProductListPagesWrapper"]/section[1]/div[2]')
+    search_results = search_table.find_elements(By.CLASS_NAME, 'product-list_ProductList__item__LiiNI')
+    results = []
+    for result in search_results:
+        link = result.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        caption = result.find_element(By.TAG_NAME, 'h3').text
+        price = result.find_elements(By.TAG_NAME, 'span')[-1].text
+        results.append((caption, price, link))
+        if len(results) == 20:
+            break
+    return results
--- Divar.py
+++ Divar.py
@@ -3,6 +3,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+class Mobile:
+    def __init__(self, link):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_argument("--headless")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(options=options)
+        driver.get(link)
+        detail_side = driver.find_element(By.CLASS_NAME, 'kt-col-5')
+        information = detail_side.find_element(By.CLASS_NAME, 'post-page__section--padded').find_elements(By.TAG_NAME, 'div')
+        self.title = detail_side.find_element(By.CLASS_NAME, 'kt-page-title__texts').find_elements(By.TAG_NAME, 'div')[0].text
+        self.brand = information[0].find_element(By.TAG_NAME, 'a').text
+        self.status = information[1].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.originality = information[2].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.sim_num = information[3].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.internal_storage = information[4].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.ram = information[5].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.color = information[6].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.price = information[7].find_elements(By.TAG_NAME, 'p')[-1].text
+        self.description = detail_side.find_elements(By.CLASS_NAME, 'post-page__section--padded')[1].find_element(By.TAG_NAME, 'p').text
+
 
 def search(target, type):
     '''this function gets a target and type of the product and
@@ -23,6 +46,7 @@ def search(target, type):
         category = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/aside/div/section[1]/ul/li[2]/ul/li[1]')
         category.click()
         sleep(1)
+        category = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/aside/div/section[1]/ul/li[2]/ul/li/ul/li[1]')
     elif type == 'laptop':
         category = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div/aside/div/section[1]/ul/li[3]')
         category.click()
@@ -67,7 +91,6 @@ def search(target, type):
     sleep(2)
     search_table = driver.find_element(By.CLASS_NAME, 'browse-post-list-f3858')
     search_results = search_table.find_elements(By.TAG_NAME, 'a')
-    sleep(3)
     results = []
     for result in search_results:
         link = result.get_attribute('href')
@@ -77,5 +100,3 @@ def search(target, type):
         if len(results) == 20:
             break
     return results
-
-print(search('پاناسونیک', 'audio'))
